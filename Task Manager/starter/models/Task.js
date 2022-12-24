293c4de26271df5990146dfbf2f61bc48eca93aa
Wesley6801 <wesleywhite424@hotmail.com>
2022-12-23 22:12:54 -0500

    Updated Files/Folders with all applicable code for use

---
+++ Task Manager/starter/models/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const TaskSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true, 'must provide name'],
+    //trim to remove things you dont want such as spaces
+        trim:true,
+        maxlength:[20, 'name cannot be more than 20 characters']
+    },
+    completed:{
+        type:Boolean,
+        default: false,
+    }
+})
+
+module.exports = mongoose.model('Task', TaskSchema)
